0534533f8148b33205c707ba3d235e055103db2a
bi1lqv <bi1lqv.y@gmail.com>
2022-10-30 06:11:54 +0000

    实验1.2

--- exp1/2.c
+++ exp1/2.c
@@ -11,11 +11,12 @@ struct birthday
   int year;
   struct list_head list;
 };
-
+static LIST_HEAD(birthday_list);
 static int birthday_init(void)
 {
-  static LIST_HEAD(birthday_list);
-  for (int i = 0; i < 5; i++)
+  struct birthday *ptr;
+  int i = 0;
+  for (; i < 5; i++)
   {
     struct birthday *person;
     person = kmalloc(sizeof(*person), GFP_KERNEL);
@@ -25,19 +26,23 @@ static int birthday_init(void)
     INIT_LIST_HEAD(&person->list);
     list_add_tail(&person->list, &birthday_list);
   }
-  struct birthday *ptr;
-  list_for_each_entry(ptr, &birthday_init, list)
+
+  list_for_each_entry(ptr, &birthday_list, list)
   {
-    printk(KERN_INFO "%d\n", *ptr->month);
+    printk(KERN_INFO "the person's birth month is %d\n", ptr->month);
   }
-  printk(KERN_INFO "Hi\n");
   return 0;
 }
 
 static void birthday_exit(void)
 {
-
-  printk(KERN_INFO "Removing Module\n");
+  struct birthday *ptr, *next;
+  list_for_each_entry_safe(ptr, next, &birthday_list, list)
+  {
+    list_del(&ptr->list);
+    kfree(ptr);
+  }
+  printk(KERN_INFO "module removed\n");
 }
 
 module_init(birthday_init);
