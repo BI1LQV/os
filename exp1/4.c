99a0ec9a110d34a9a45200f65c922366a3c51e71
bi1lqv <bi1lqv.y@gmail.com>
2022-10-30 12:56:13 +0000

    历史存不好

--- exp1/4.c
+++ exp1/4.c
@@ -25,9 +25,14 @@ char getLast(char *word)
   return -1; //正常单词
 }
 
+char **history[20] = {0}; //懒得自己维护lru了 每次满了往前腾10个空就行
+int historyPtr = 0;
+int count = 0;
 int main()
 {
-  char *args[MAX_LINE / 2 + 1];
+  char **args = (char **)malloc(sizeof(char *) * MAX_LINE);
+  char **argsBackup = (char **)malloc(sizeof(char *) * MAX_LINE);
+  int bkPtr = 0;
   int should_run = 1;
   while (should_run)
   {
@@ -39,23 +44,37 @@ int main()
     int isBackground = 0; //判断是否后台运行
     do
     {
-      char *word = (char *)malloc(MAX_LINE); //每个单词的大小 共用maxline好了
-      scanf("%s", word);                     //读入一个词
-      c = getchar();                         //吃一个空格
+      char *word = (char *)malloc(sizeof(char) * MAX_LINE);       //每个单词的大小 共用maxline好了
+      char *wordBackup = (char *)malloc(sizeof(char) * MAX_LINE); //备份
+      scanf("%s", word);                                          //读入一个词
+      c = getchar();                                              //吃一个空格
       args[argsPtr++] = word;
-      if (strcmp(word, "exit") == 0)
+      if (strcmp(word, "exit") == 0) // exit退出
       {
         should_run = 0;
-      } // exit退出
-
-      int endAt = getLast(word);
-      if (endAt != -1)
-      {
-        //结尾是&，去掉&
-        //如果输入的是 xxx & 那args是["xxx",""]应该也是正确的
-        word[endAt] = '\0';
-        isBackground = 1;
       }
+      else if (strcmp(word, "history") == 0)
+      {
+        for (int p = historyPtr - 1; p > historyPtr - 10 && p >= 0; p--)
+        {
+          // printf("%d\n", p);
+          printf("[%d]%s\n", count - (historyPtr - p), history[p][0]);
+        }
+      }
+      else
+      {
+        int endAt = getLast(word);
+        if (endAt != -1)
+        {
+          //结尾是&，去掉&
+          //如果输入的是 xxx & 那args是["xxx",""]应该也是正确的
+          word[endAt] = '\0';
+          isBackground = 1;
+        }
+        memcpy(wordBackup, word, sizeof(char) * MAX_LINE);
+        argsBackup[bkPtr++] = wordBackup;
+      }
+
     } while (c == ' ');
 
     args[argsPtr] = NULL; //给个结束符
@@ -82,6 +101,23 @@ int main()
         waitpid(id, &status, 0); //阻塞主线程
       }
     }
+    history[historyPtr++] = argsBackup;
+    argsBackup = (char **)malloc(sizeof(char *) * MAX_LINE);
+
+    // history[historyPtr++] = args;
+    // if (historyPtr == 19)
+    // { // history满了
+    //   for (int p = 0; p < 10; p++)
+    //   {
+    //     free(history[p]);
+    //   }
+    //   for (int p = 0; p < 10; p++)
+    //   {
+    //     history[p] = history[p + 10];
+    //   }
+    //   historyPtr = 10;
+    // }
+    count++;
   }
   return 0;
 }
