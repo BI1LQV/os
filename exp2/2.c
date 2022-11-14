e140db7ec119e776c66c7278196bdd9061f9bb87
bi1lqv <bi1lqv.y@gmail.com>
2022-11-14 15:11:44 +0800

    静态优化

--- exp2/2.c
+++ exp2/2.c
@@ -7,13 +7,13 @@
 #include <omp.h>
 
 #include "const.h"
-int generates(int total)
+int generates(int total, unsigned int *seed)
 {
   int count = 0;
   for (int i = 0; i < total; i++)
   {
-    float x = rand() / (double)RAND_MAX;
-    float y = rand() / (double)RAND_MAX;
+    float x = rand_r(seed) / (double)RAND_MAX;
+    float y = rand_r(seed) / (double)RAND_MAX;
     count += x * x + y * y < 1;
   }
   return count;
@@ -29,18 +29,34 @@ double pi = 0;
 void calc()
 {
   int res[TOTAL_THREADS] = {0};
-  while (fabs(pi - ACC_PI) > DELTA && total < 2e8)
-  {
+  int totalCount[TOTAL_THREADS] = {0};
+  unsigned int seed = omp_get_thread_num();
+
 #pragma omp parallel num_threads(TOTAL_THREADS)
+  {
+    int *resTarget = res + omp_get_thread_num();
+    int *totalTarget = totalCount + omp_get_thread_num();
+    // int *resSnapshot = malloc(sizeof(int) * TOTAL_THREADS);
+    // int *totalSnapshot = malloc(sizeof(int) * TOTAL_THREADS);
+    while (fabs(pi - ACC_PI) > DELTA && *totalTarget < 2e8 / TOTAL_THREADS)
     {
-      res[omp_get_thread_num()] = generates(BLOCK_SIZE);
+      *resTarget += generates(BLOCK_SIZE, &seed);
+      *totalTarget += BLOCK_SIZE;
+      // memcpy(resSnapshot, res, sizeof(int) * TOTAL_THREADS);
+      // memcpy(totalSnapshot, totalCount, sizeof(int) * TOTAL_THREADS);
+      // int curInside = 0;
+      // int curTotal = 0;
+      // for (int i = 0; i < TOTAL_THREADS; i++)
+      // {
+      //   curInside += resSnapshot[i];
+      //   curTotal += totalSnapshot[i];
+      // }
+      // curInside += inside;
+      // curTotal += total;
+      // total = curTotal;
+      // inside = curInside;
+      // pi = 4 * curInside / (double)curTotal;
     }
-    total += BLOCK_SIZE * TOTAL_THREADS;
-    for (int i = 0; i < TOTAL_THREADS; i++)
-    {
-      inside += res[i];
-    }
-    pi = 4 * inside / (double)total;
   }
 }
 
@@ -49,13 +65,7 @@ int main()
   struct timeval t1, t2;
   gettimeofday(&t1, NULL);
 
-  srand(SEED);
-  omp_set_max_active_levels(2);
-
-#pragma omp parallel num_threads(TOTAL_THREADS)
-  {
-    calc();
-  }
+  calc();
 
   gettimeofday(&t2, NULL);
   printf("pi=%.10f; inside=%d; outside=%d\n", pi, inside, total);
--- exp2/static.c
+++ exp2/static.c
@@ -4,14 +4,13 @@
 #include <time.h>
 #include <sys/time.h>
 
-#define TOTAL_THREADS 32
-#define TOTAL_POINTS 100000000
+#define TOTAL_THREADS 8
+#define TOTAL_POINTS (int)2e8
 
 int generates(int total)
 {
   int inside = 0;
-  unsigned int seed = (unsigned int)omp_get_thread_num();
-  // printf("%d\n", seed);
+  unsigned int seed = omp_get_thread_num();
   for (int i = 0; i < total; i++)
   {
     float x = rand_r(&seed) / (double)RAND_MAX;
@@ -29,8 +28,7 @@ int main()
   struct timeval t1, t2;
   gettimeofday(&t1, NULL);
   int inside[TOTAL_THREADS] = {0};
-  srand((unsigned)time(NULL));
-  int totalPoints = 100000000;
+
 #pragma omp parallel num_threads(TOTAL_THREADS)
   {
     inside[omp_get_thread_num()] = generates(TOTAL_POINTS / TOTAL_THREADS);
@@ -42,7 +40,7 @@ int main()
     sum += inside[i];
   }
   gettimeofday(&t2, NULL);
-  printf("%f\n", 4 * sum / (float)TOTAL_POINTS);
+  printf("pi=%.10f; inside=%d; outside=%d\n", 4 * sum / (double)TOTAL_POINTS, sum, TOTAL_POINTS);
   double timeCost = (t2.tv_sec - t1.tv_sec) * 1000.0 + (t2.tv_usec - t1.tv_usec) / 1000.0;
   printf("time cost: %f ms\n", timeCost);
   return 0;
